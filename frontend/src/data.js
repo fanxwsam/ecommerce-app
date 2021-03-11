8a74f16a27453396b45e8626fd1ce6472c685a3f
fanxwsam <fanxwsam@gmail.com>
2021-03-12 07:26:15 +0800

    init

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: './images/p1.jpg',
+            price: 80,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+
+        {
+            _id: '2',
+            name: 'Addidas Slim Shirt',
+            category: 'Shirts',
+            image: './images/p1.jpg',
+            price: 120,
+            brand: 'Adidas',
+            rating: 5,
+            numReviews: 13,
+            description: 'high quality product'
+        },
+
+        {
+            _id: '3',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: './images/p3.jpg',
+            price: 330,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+
+        {
+            _id: '4',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: './images/p1.jpg',
+            price: 450,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+
+        {
+            _id: '5',
+            name: 'Puma Slim Shirt',
+            category: 'Shirts',
+            image: './images/p1.jpg',
+            price: 19,
+            brand: 'Puma',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+
+        {
+            _id: '6',
+            name: 'Tiffiny Slim Pantss',
+            category: 'Shirts',
+            image: './images/p6.jpg',
+            price: 350,
+            brand: 'Tiffiny',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        }
+    ]
+};
+
+export default data;
