71e1a740b7205ab0baeee16a97ed47ab33f6bff3
fanxwsam <fanxwsam@gmail.com>
2021-03-15 12:05:51 +0800

    Add Product Details Page

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -4,84 +4,91 @@ const data = {
             _id: '1',
             name: 'Nike Slim Shirt',
             category: 'Shirts',
-            image: './images/p1.jpg',
+            image: '/images/p1.jpg',
             price: 80,
             brand: 'Nike',
             rating: 4.5,
             numReviews: 10,
-            description: 'high quality product'
+            description: 'high quality product',
+            countInStock: 0
         },
 
         {
             _id: '2',
             name: 'Addidas Slim Shirt',
             category: 'Shirts',
-            image: './images/p1.jpg',
+            image: '/images/p2.jpg',
             price: 120,
             brand: 'Adidas',
             rating: 5,
             numReviews: 13,
-            description: 'high quality product'
+            description: 'high quality product',
+            countInStock: 10
         },
 
         {
             _id: '3',
             name: 'Nike Slim Shirt',
             category: 'Shirts',
-            image: './images/p3.jpg',
+            image: '/images/p3.jpg',
             price: 330,
             brand: 'Nike',
             rating: 4.5,
             numReviews: 10,
-            description: 'high quality product'
+            description: 'high quality product',
+            countInStock: 10
         },
 
         {
             _id: '4',
             name: 'Nike Slim Shirt',
             category: 'Shirts',
-            image: './images/p1.jpg',
+            image: '/images/p1.jpg',
             price: 450,
             brand: 'Nike',
             rating: 4.5,
             numReviews: 10,
-            description: 'high quality product'
+            description: 'high quality product',
+            countInStock: 10
         },
 
         {
             _id: '5',
             name: 'Puma Slim Shirt',
             category: 'Shirts',
-            image: './images/p1.jpg',
+            image: '/images/p1.jpg',
             price: 19,
             brand: 'Puma',
             rating: 5,
             numReviews: 10,
-            description: 'high quality product'
+            description: 'high quality product',
+            countInStock: 10
         },
 
         {
             _id: '6',
             name: 'Tiffiny Slim Pantss',
             category: 'Shirts',
-            image: './images/p6.jpg',
+            image: '/images/p6.jpg',
             price: 350,
             brand: 'Tiffiny',
             rating: 4.5,
             numReviews: 10,
-            description: 'high quality product'
+            description: 'high quality product',
+            countInStock: 10
         },
 
         {
             _id: '7',
             name: 'Tiffiny Slim Pantss',
             category: 'Shirts',
-            image: './images/p6.jpg',
+            image: '/images/p6.jpg',
             price: 350,
             brand: 'Tiffiny',
-            rating: 4.5,
+            rating: 3.5,
             numReviews: 10,
-            description: 'high quality product'
+            description: 'high quality product',
+            countInStock: 10
         }
     ]
 };
